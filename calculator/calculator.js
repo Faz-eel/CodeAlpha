50dd8a0d5a151a1304e3e1625076917200975a62
Fazil EAP <yiahhuaru@gmail.com>
2025-03-02 17:31:12 +0000

    added pseudocode & responsive styling

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,5 +1,5 @@
-// show the value of a key on the screen when pressed
 document.addEventListener('keydown', function(event) {
+    // show the value of a number key on the screen when using the keyboard
     if (event.key == '0' || event.key == '1' 
         || event.key == '2' || event.key == '3' 
         || event.key == '4' || event.key == '5' 
@@ -16,45 +16,51 @@ document.addEventListener('keydown', function(event) {
     }
 })
 
-document.addEventListener('keydown', function(event) {
-    if (event.key == 'enter') {
-        operate();
-    }
-})
 
 function backspace() {
+    // convert screen value to string and slice it by 1
     screen = document.querySelector('#screen').value;
     result = screen.toString();
     y = result.slice(0, result.length-1);
     document.querySelector('#screen').value = y;
 }
 
+
 function operate() {
     screen = document.querySelector('#screen').value;
     
+    // evaluate expression on screen and store the answer in local storage
     try {
         result = math.evaluate(screen);
         localStorage.setItem('Ans', result);
         document.querySelector('#result').value = `=${result}`;
     }
+
+    // catch any errors and display error message
     catch(err) {
         console.log(err.Name);
-        document.querySelector('#result').value = 'SYNTAX ERROR';
+        document.querySelector('#result').value = 'MATH ERROR';
     }      
 }  
 
+
 function show(value) {
+    // append the text on the display screen
     screen = document.querySelector('#screen');
     screen.value += value;
 }
 
+
 function prev_answer() {
+    // retrieve previously stored answer
     previous = localStorage.getItem('Ans');
     document.querySelector('#screen').value = previous;
     document.querySelector('#result').value = '';
 }
 
+
 function clear_screen() {
+    // clear the screen
     document.querySelector('#screen').value = '';
     document.querySelector('#result').value = '';
 }
